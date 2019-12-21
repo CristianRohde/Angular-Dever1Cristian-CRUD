e4b4e585ea659e2fa41db83f29d59601de376d0e
Cristian Rohde <cristianedis@gmail.com>
2019-12-20 23:14:22 -0300

    Up Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Empleado} from './models/empleado'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Dever1Cristian-CRUD';
+  empleadoArray: Empleado[]=[
+    {id: 1, nome: "Cristian", nacionalidade:"Argentina"},
+    {id: 2, nome: "Valeria", nacionalidade:"Argentina"},
+    {id: 3, nome: "Arthur", nacionalidade:"Brasil"},
+  ];
+
+  selecionarEmpleado: Empleado = {id:0, nome: '', nacionalidade: ''};
+
+  openForEdit(empleado: Empleado): void 
+  {
+    this.selecionarEmpleado = empleado;
+  }
+
+  addOrEdit(): void
+  {
+    if(this.selecionarEmpleado.id === 0) // INSERT
+    {
+      this.selecionarEmpleado.id = this.empleadoArray.length + 1;
+      this.empleadoArray.push(this.selecionarEmpleado);
+    }
+    this.selecionarEmpleado = {id:0, nome: '', nacionalidade: ''};
+  }
+
+  delete(): void 
+  {
+    if(confirm('Tem certeza de que deseja excluí-lo?'))
+    {
+      this.empleadoArray = this.empleadoArray.filter(x => x != this.selecionarEmpleado);
+      this.selecionarEmpleado = {id:0, nome: '', nacionalidade: ''};
+    }
+  }
+
 }
